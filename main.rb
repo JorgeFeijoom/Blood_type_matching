a42e5498846b2a6ef145ee7437a62d9aafaf9509
Jorge Feijoo Melian <feijoo_melian-j@ulster.ac.uk>
2018-11-16 11:54:25 +0000

    Updated data files + argument -t:  elapsed time

---
+++ main.rb
@@ -0,0 +1,16 @@
+class MAIN
+  require_relative 'PROCESS'
+
+  $t1 = Time.now  #Init procedure timestamp
+
+  c = PROCESS.new #Run process
+  c.read
+
+  $t2 = Time.now
+
+  ARGV.each do|a| # -t argument for elapsed time
+    if a == "-t"
+      $msecs = c.time_diff_milli $t1, $t2
+    end
+  end
+end
--- process.rb
+++ process.rb
@@ -8,61 +8,48 @@ class PROCESS
         "X" => ["C", "X"], # AB-
         "O" => ["A", "B", "C", "O"], # O+
         "W" => ["A", "Z", "B", "Y", "C", "X", "O", "W"]} # O-
-  $ga, $gz, $gb , $gy, $gc, $gx, $go, $gw = [], [], [], [], [], [], [], []
-  $group_type = {}
+  $ga, $gz, $gb , $gy, $gc, $gx, $go, $gw = [], [], [], [], [], [], [], [] #Ini arrays same group
+  $group_type = {} # Ini hash for divided patients by blood types
+  $register = 0
 
   def read
-    $t1 = Time.now
-
-    #person = Hash.new
     person = []
-    IO.foreach("dataset.txt") do |line|
-      # Remove trailing whitespace.
-      line.chomp!
-      #Saving data
-      person.push(line.split(":"))
+    IO.foreach("data_50.txt") do |line| #Get all patients from file iterate each line
+      line.chomp!  # Remove trailing whitespace.
+      person.push(line.split(":"))  #Saving data split :
     end
-    blood(person)
-  end
-
-  def blood(person)
-    person = List.new(person)
-    person_match = []
-    person.each do |p|
-       person_match.push(matches(p.last))
-    end
-    group(person)
+    group(person) #Get blood type
   end
 
   def matches(b)
-     $blood_compatibility[b]
+     $blood_compatibility[b] #Get compatibility from each blood type
   end
 
   def group(person)
-    person.each do |p|
+    person.each do |p|  #Patient iterator
       type = p.last
-      case type
-      when  "A"
-        $ga.push(p)
-      when  "Z"
-        $gz.push(p)
-      when  "B"
-        $gb.push(p)
-      when  "Y"
-        $gy.push(p)
-      when  "C"
-        $gc.push(p)
-      when  "X"
-        $gx.push(p)
-      when  "O"
-        $go.push(p)
-      when  "W"
-        $gw.push(p)
-      else
-        break
+      case type #Divide patients in groups type
+        when  "A"
+          $ga.push(p)
+        when  "Z"
+          $gz.push(p)
+        when  "B"
+          $gb.push(p)
+        when  "Y"
+          $gy.push(p)
+        when  "C"
+          $gc.push(p)
+        when  "X"
+          $gx.push(p)
+        when  "O"
+          $go.push(p)
+        when  "W"
+          $gw.push(p)
+        else
+          break
       end
-
     end
+    #Save divided patients in a Hash with key-> Blood type
     $group_type["A"] = $ga
     $group_type["Z"] = $gz
     $group_type["B"] = $gb
@@ -80,10 +67,12 @@ class PROCESS
     person.each do |p|
       name = []
       match = matches(p.last)
-
+      if match.nil?
+        break
+      end
       match.each do |m|
         $group_type[m].each do |gp|
-          name.push(gp)
+          name.push(gp.first)
         end
       end
       receivers_list[p.first] = name
@@ -93,55 +82,28 @@ class PROCESS
   end
 
   def combinations(receivers_list)
-    hash_names = {}
-    receivers_list.each do |donor,receivers|
-      list_of_names = []
-      receivers.each do |r|
-        list_of_names.push(r)
-        hash_names[donor] = list_of_names
-      end
-    end
-    combinations_iteration(hash_names)
-  end
-
-  def combinations_iteration(hsh)
-    #print hsh
-    receivers_h = {}
-    hsh.each do |key, attrs|
-      receivers = []
-      puts "Donor:  #{key} | Receivers:  #{attrs}."
-      attrs.each do |p|
-        receivers.push(p.first)
-      end
-      receivers_h[key] = receivers
-    end
-    receivers_h.each do |key, attrs|
+    receivers_list.each do |key, attrs|
+      puts "\n Donor:  #{key} "
+      print  "Receivers: "
       comb = [[key],attrs]
-      puts "\n Donor: #{key} \n"
-      puts variations(comb).join(' ')
-      puts "\n"
+      puts combinations_iterator(comb).join(' ')
     end
-
+    puts "\n\nTotal combinations: #{$register}"
   end
-  def variations(a)
+
+  def combinations_iterator(a)
     first = a.first
     if a.length==1 then
       first
     else
-      rest = variations(a[1..-1])
-      first.map{ |x| rest.map{ |y| "#{x}#{y}" } }.flatten
+      rest = combinations_iterator(a[1..-1])
+      map = first.map{rest.map{ |y| "#{y}," } }.flatten
+      $register += map.size
+      return map
     end
   end
 
   def time_diff_milli(start, finish)
-    puts (finish - start) * 1000.0
+    puts "\nElapsed time: #{(finish - start) * 1000.0} ms"
   end
 end
-
-c = PROCESS.new
-c.read
-
-$t2 = Time.now
-$msecs = c.time_diff_milli $t1, $t2
-
-
