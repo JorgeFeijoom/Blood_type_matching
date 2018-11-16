5b0ebd198456d073ffc3ef9699cd481827cfa18c
Jorge Feijoo Melian <feijoo_melian-j@ulster.ac.uk>
2018-11-16 22:51:26 +0000

    fixed problems

--- process.rb
+++ process.rb
@@ -8,8 +8,9 @@ class PROCESS
         "X" => ["C", "X"], # AB-
         "O" => ["A", "B", "C", "O"], # O+
         "W" => ["A", "Z", "B", "Y", "C", "X", "O", "W"]} # O-
-  $last_patient = ""
-  $register = 0
+  $ga, $gz, $gb , $gy, $gc, $gx, $go, $gw = [], [], [], [], [], [], [], [] # Ini arrays same group
+  $group_type = {} # Ini hash for divided patients by blood types
+  #$register = 0
 
   def read
     $t1 = Time.now  # Init procedure timestamp
@@ -18,40 +19,83 @@ class PROCESS
       line.chomp!  # Remove trailing whitespace.
       person.push(line.split(":"))  # Saving data split :
     end
-    get_combinations(person) # Get blood type
+    group(person) # Get blood type
   end
 
-  # Checks blood compatibility between two persons
-  def check_compatibility(patient1_blood, patient2_blood)
-      $blood_compatibility[patient1_blood].each do |blood|
-        if blood == patient2_blood
-            return true
+  def matches(b)
+     $blood_compatibility[b] # Get compatibility from each blood type
+  end
+
+  def group(person)
+    person = List.new(person)
+    person.iterator do |p|  # Patient iterator
+      type = p.last
+      case type # Divide patients in groups type
+        when  "A"
+          $ga.push(p)
+        when  "Z"
+          $gz.push(p)
+        when  "B"
+          $gb.push(p)
+        when  "Y"
+          $gy.push(p)
+        when  "C"
+          $gc.push(p)
+        when  "X"
+          $gx.push(p)
+        when  "O"
+          $go.push(p)
+        when  "W"
+          $gw.push(p)
+        else
+          break
+      end
+    end
+    # Save divided patients in a Hash with key-> Blood type
+    $group_type["A"] = $ga
+    $group_type["Z"] = $gz
+    $group_type["B"] = $gb
+    $group_type["Y"] = $gy
+    $group_type["C"] = $gc
+    $group_type["X"] = $gx
+    $group_type["O"] = $go
+    $group_type["W"] = $gw
+
+    receivers(person)
+  end
+
+  def receivers(person)
+    receivers_list = {}
+    person.iterator do |p| # Patients iteration
+      name = []
+      match = matches(p.last) # Search blood matches
+      match.each do |m|
+        $group_type[m].each do |gp| # Blood type iterator of each patient
+          name.push(gp)
         end
       end
+      receivers_list[p] = name # Save Hash with: (Patient donor)->[receivers]
+    end
+
+    combinations(receivers_list)
   end
 
-# Gets the list of all compatible persons
-  def get_compatibility(combination)
-      patient1 = combination[0]
-      patient2 = combination[1]
-      p1_blood = patient1[(patient1).size-1]
-      p2_blood = patient2[(patient2).size-1]
-      $last_patient
-      if check_compatibility(p1_blood, p2_blood)
-        if $last_patient != patient1
-          $last_patient = patient1
-          print"\n\nPatient #{patient1} can donate to:\n\n"
-        end
-        print "#{patient2} \n"
-      end
+  def combinations(receivers_list) # Receivers combinations of each blood donor
+    receivers_list.each do |key, attrs|
+      puts "\nPatient #{key} can donate to:"
+      comb = [[key],attrs]
+      puts "\n #{combinations_iterator(comb).join(' ')}"
+    end
   end
 
-
-# Gets all combinations without repetition of the given persons
-  def get_combinations(persons)
-      persons.combination(2).each do |comb|
-        get_compatibility(comb)
-      end
+  def combinations_iterator(a)
+    first = a.first # Get first receiver to start
+    if a.length==1 # If only one receiver
+      first
+    else
+      rest = combinations_iterator(a[1..-1]) # Receivers iteration
+      first.map{rest.map{ |y| "#{y}," } }.flatten # Map generator donor with receivers
+    end
   end
 
   def time_diff_milli(start, finish) # Elapsed time
